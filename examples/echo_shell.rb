8d05e89b01d66fd74449d52f559a3aba8846b893
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-09 19:28:21 +0900

    feat: example echo_shell uses live slot for /slow and custom style

--- examples/echo_shell.rb
+++ examples/echo_shell.rb
@@ -3,8 +3,24 @@
 require "cclikesh"
 
 Cclikesh.run do |shell|
+  shell.define_style(:warn, fg: :yellow, bold: true)
+
   shell.on_submit do |line, ctx|
     ctx.display.append("you said: #{line}", style: :result)
+  end
+
+  shell.slash(:slow) do |_args, ctx|
+    ctx.display.open_live(style: :thinking) do |slot|
+      3.times do |i|
+        sleep 0.1
+        slot.update("Roosting... #{i + 1}/3")
+      end
+    end
+    ctx.display.append("done", style: :result)
+  end
+
+  shell.slash(:warn) do |args, ctx|
+    ctx.display.append(args.join(" "), style: :warn)
   end
 
   shell.slash(:quit) do |_args, ctx|
