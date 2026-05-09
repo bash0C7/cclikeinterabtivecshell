178765c054818a8f05c5bf51fd9b8ba3e4d1013c
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-09 18:02:23 +0900

    refactor: rewrite Runner to fork impl/F and run reline + render threads

--- lib/cclikesh/runner.rb
+++ lib/cclikesh/runner.rb
@@ -1,31 +1,48 @@
 # frozen_string_literal: true
 
+require "drb/drb"
+require "drb/unix"
+require "reline"
 require_relative "tuple_space"
 require_relative "builder"
 require_relative "context"
 require_relative "dispatcher"
-require_relative "render_ractor"
-require_relative "input_ractor"
+require_relative "handler_registry"
+require_relative "forking"
+require_relative "render_thread"
+require_relative "input_thread"
 
 module Cclikesh
   class Runner
-    def self.run(input_path:, output_path:, tick_interval: 0.06, &block)
+    def self.run(tick_interval: 0.06, &block)
       builder = Builder.new
       block.call(builder)
+      registry = HandlerRegistry.new(builder)
+
+      Forking.run(registry) do |handlers_uri|
+        run_child(handlers_uri, tick_interval: tick_interval)
+      end
+    end
+
+    def self.run_child(handlers_uri, tick_interval:)
+      DRb.start_service
+      registry_remote = DRbObject.new_with_uri(handlers_uri)
 
       ts = TupleSpace.new
       ctx = Context.new(ts)
-      dispatcher = Dispatcher.new(ts, builder, ctx)
+      dispatcher = Dispatcher.new(ts, registry_remote, ctx)
 
-      render_ractor = RenderRactor.start(ts, output_path, tick_interval: tick_interval)
-      InputRactor.start(ts, input_path)
+      render_thread = RenderThread.start(ts, $stdout, tick_interval: tick_interval)
+      input_thread  = InputThread.start(ts, reader: Reline.method(:readline), prompt: "> ")
 
       loop do
         break if dispatcher.dispatch_one == :quit
       end
 
       ts.write([:cmd, :quit])
-      render_ractor.value rescue nil
+      render_thread.join(2)
+      input_thread.join(2)
+      DRb.stop_service
     end
   end
 end
