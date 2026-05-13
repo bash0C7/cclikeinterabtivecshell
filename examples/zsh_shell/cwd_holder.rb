0a3e01f96958da560fd459886d8e85ef771a3f2e
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-13 20:52:35 +0900

    feat(example): add CwdHolder for zsh_shell shareable_ref(:cwd)

---
+++ examples/zsh_shell/cwd_holder.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class CwdHolder
+  def initialize
+    @initial = Dir.pwd
+    @cwd = @initial
+  end
+
+  def pwd
+    @cwd
+  end
+
+  def cd(path)
+    target =
+      if path.nil? || path.empty? || path == "~"
+        ENV["HOME"]
+      else
+        File.expand_path(path, @cwd)
+      end
+    raise Errno::ENOENT, target unless Dir.exist?(target)
+    @cwd = target
+    true
+  end
+
+  def reset
+    @cwd = @initial
+    true
+  end
+end
---
+++ examples/zsh_shell/test/test_cwd_holder.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "test/unit"
+require "tmpdir"
+require_relative "../cwd_holder"
+
+class TestCwdHolder < Test::Unit::TestCase
+  def setup
+    @holder = CwdHolder.new
+  end
+
+  def test_pwd_returns_current_directory
+    assert_equal Dir.pwd, @holder.pwd
+  end
+
+  def test_cd_to_existing_directory
+    Dir.mktmpdir do |tmp|
+      @holder.cd(tmp)
+      assert_equal File.realpath(tmp), File.realpath(@holder.pwd)
+    end
+  end
+
+  def test_cd_nil_goes_home
+    @holder.cd(nil)
+    assert_equal ENV["HOME"], @holder.pwd
+  end
+
+  def test_cd_empty_goes_home
+    @holder.cd("")
+    assert_equal ENV["HOME"], @holder.pwd
+  end
+
+  def test_cd_tilde_goes_home
+    @holder.cd("~")
+    assert_equal ENV["HOME"], @holder.pwd
+  end
+
+  def test_cd_nonexistent_raises_enoent
+    assert_raise(Errno::ENOENT) { @holder.cd("/no/such/path/please") }
+  end
+
+  def test_cd_relative_resolves_from_current_cwd
+    Dir.mktmpdir do |tmp|
+      sub = File.join(tmp, "sub")
+      Dir.mkdir(sub)
+      @holder.cd(tmp)
+      @holder.cd("sub")
+      assert_equal File.realpath(sub), File.realpath(@holder.pwd)
+    end
+  end
+
+  def test_reset_restores_initial
+    initial = @holder.pwd
+    Dir.mktmpdir do |tmp|
+      @holder.cd(tmp)
+      @holder.reset
+      assert_equal initial, @holder.pwd
+    end
+  end
+end
